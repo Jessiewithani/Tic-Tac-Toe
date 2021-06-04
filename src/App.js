3755a15b60dff9a69bbbfe124402a72409f1c907
Jessie Le-Ho <lehoj@oregonstate.edu>
2021-06-04 16:49:58 -0600

    Create and setup layout of Square component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Board from './components/Board';
 
 function App() {
+
   return (
     <div className="App">
       <div className="game-container">
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,22 @@
 import { useState } from 'react';
+import Square from '../components/Square';
 
 function Board() {
+
+  const [ board, setBoard ] = useState(Array(9).fill(null));
+
+  const square = board.map((el, index) => 
+    <Square 
+    key={index} 
+    value={el}  />
+  );
+
   return (
+
+
     <div className="board">
-      <h3>BOARD</h3>
+      <h3 className="next-player-message">Next player!</h3>
+      {square}
     </div>
   )
 }

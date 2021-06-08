7a96f91b4e575d60a0a4ce4d1f4add44e9691dab
Jessie Le-Ho <lehoj@oregonstate.edu>
2021-06-07 22:42:14 -0600

    Create function to check winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,18 +6,45 @@ function Board() {
 
   const [ board, setBoard ] = useState(Array(9).fill(null));
   const [ player, setPlayer ] = useState('X');
+  const [ winner, setWinner ] = useState(null);
+
+  const checkWinner = () => {
+
+    let winningCombos = [
+      [0,1,2],
+      [4,5,6],
+      [7,8,9],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+
+    for (var i = 0; i < winningCombos.length; i++) {
+      const[a,b,c] = winningCombos[i];
+
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        setWinner(player)
+        console.log('player', player)
+        break;
+      }
+    }
+  }
 
   const handleClick = (index) => {
     console.log('i', index)
     let newBoard = board;
 
-    if (board[index] === null) {
+    if (board[index] === null && !winner) {
       newBoard[index] = player;
 
       let newPlayer = player === 'X' ? 'O' : 'X';
 
       setBoard(newBoard);
       setPlayer(newPlayer);
+
+      checkWinner();
     }
 
   }
@@ -33,7 +60,7 @@ function Board() {
 
 
     <div className="board">
-      <h3 className="next-player-message">Next player!</h3>
+      <h3 className="next-player-message">{!winner ? `${player}'s turn!` : `${winner} won!`}</h3>
       {square}
     </div>
   )

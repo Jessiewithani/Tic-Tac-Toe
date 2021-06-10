8b6c0350c23ee8ab2799b6760ea0d6b89ba75d7d
Jessie Le-Ho <lehoj@oregonstate.edu>
2021-06-09 23:00:39 -0600

    Set up squares in Opponent component

--- src/components/Opponent.js
+++ src/components/Opponent.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Square from '../components/Square';
+import '../Opponent.css';
 
 function Opponent() {
   return(
-    <div>
-      <h3>OPPONENT COMPONENT!</h3>
+    //we would need onclick functions for these two squares
+    //we can try to add the props from the child to the parent with a callback function
+    <div className="opponent-container">
+      <Square value="X"  />
+      <Square value="O"  />
     </div>
   )
 }

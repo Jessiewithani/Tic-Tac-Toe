ad6c7717a507d5d3d2b4aae738136db5728be320
Jessie Le-Ho <lehoj@oregonstate.edu>
2021-06-09 22:43:45 -0600

    Create Opponent component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Board from './components/Board';
+import Opponent from './components/Opponent';
 
 function App() {
 
   return (
     <div className="App">
       <div className="game-container">
+        <Opponent />
         <Board />
       </div>
     </div>
---
+++ src/components/Opponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Opponent() {
+  return(
+    <div>
+      <h3>OPPONENT COMPONENT!</h3>
+    </div>
+  )
+}
+
+export default Opponent;

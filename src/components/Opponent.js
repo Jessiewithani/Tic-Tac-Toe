945146b4c343f20222a04443457ea08d91bcd99e
Jessie Le-Ho <lehoj@oregonstate.edu>
2021-06-10 22:20:39 -0600

    Think of what the next steps are for the Opponent component

--- src/components/Opponent.js
+++ src/components/Opponent.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Square from '../components/Square';
 import '../Opponent.css';
 
 function Opponent() {
+
+  // const [player, setPlayer] = useState(null)
+
+  // useEffect(() => {
+  //   if (!player === null) {
+  //     setPlayer(the square's value)
+  //   }
+  // })
+  
   return(
     //we would need onclick functions for these two squares
+    //should I do a useEffect for handling the onclick for these values??
     //we can try to add the props from the child to the parent with a callback function
     <div className="opponent-container">
       <Square value="X"  />
